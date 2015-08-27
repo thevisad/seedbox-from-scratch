9f32cb0d210680680f050c8b536f8c8a1160e558
dannyti <necrosmite@yahoo.in>
2015-08-27 06:58:07 +0530

    update

--- RDPconfig.sh
+++ RDPconfig.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-
-# Automatic RDP session configurator
-
-# set LANG so that dpkg etc. return the expected responses so the script is guaranteed to work under different locales
 export LANG="C"
 
-###########################################################
-# Before doing anything else, check if we're running with #
-# priveleges, because we need to be.                      #
-###########################################################
 id=`id -u`
 if [ $id -ne 0 ]
 	then
 		clear
-		echo "You tried running the ScaryGliders RDPsesconfig utility as a non-priveleged user. Please run as root."
+		echo "You tried running the Desktop User Config as a non-priveleged user. Please run with sudo."
 		exit 1
 fi
 
@@ -22,25 +14,13 @@ fi
 #################################################################
 
 INTERACTIVE=1
+Identify Distro
+Dist=`lsb_release -d -s` 
 
-Dist=`lsb_release -d -s` # What are we running on
-
-backtitle="Scarygliders RDPsesconfig"
-questiontitle="RDPsesconfig Question..."
-title="RDPsesconfig"
+backtitle="Mate Desktop RDP Config"
+questiontitle="Which Users..."
+title="Configurator"
 DIALOG="dialog"
-
-# Check for running on supported/tested Distros...
-supported=0
-while read i
-do
-	if [ "$Dist" = "$i" ]
-	then
-		supported=1
-		break
-	fi
-done < SupportedDistros.txt
-
 
 ###############################################
 # Text/dialog front-end function declarations #
@@ -67,57 +47,7 @@ apt_update_interactive()
 }
 
 
-
-
-#############################################
-# Common function declarations begin here...#
-#############################################
-
-install_package_interactive()
-{
-	debconf-apt-progress --dlwaypoint 50 -- apt-get -y install $PkgName
-	sleep 1 # Prevent possible dpkg race condition (had that with Xubuntu 12.04 for some reason)
-}
-
-# Interrogates dpkg to find out the status of a given package name, and installs if needed...
-check_package()
-{
-	DpkgStatus=`dpkg-query -s $PkgName 2>&1`
-
-	case "$DpkgStatus" in
-		*"is not installed and no info"*)
-			PkgStatus=0
-			# "Not installed."
-			;;
-		*"deinstall ok config-files"*)
-			PkgStatus=1
-			# "Deinstalled, config files are still on system."
-			;;
-		*"install ok installed"*)
-			PkgStatus=2
-			# "Installed."
-			;;
-	esac
-
-  if [ "$PkgStatus" = "0" ] || [ $PkgStatus = "1" ] # Install or re-install package and give a relatively nice-ish message whilst doing so - Zenity is kind of limited...
-	then
-		install_package_interactive
-	fi
-}
-
-# Check for necessary packages and install if necessary...
-install_required_packages()
-{
-  for PkgName in ${RequiredPackages[@]}
-  do
-  	check_package
-  done
-}
-
-# Reads all normal (non-system) accounts in from /etc/passwd, and presents them as a list
-# If your system also has "machine accounts" (i.e. accounts for PC's to be added under a
-# SAMBA domain controller - marked with a "$" at the end) these will be ignored.
-# smbguest is also ignored.
+# Reads all normal (non-system) accounts 
 select_local_user_accounts_to_config()
 {
 	if [ -e ./usernames.tmp ]
@@ -172,7 +102,7 @@ select_local_user_accounts_to_config()
 	done < ./usernames.tmp
    
 	windowsize=(0 0 0)
-	dialog_param=("--separate-output" "--backtitle" "$backtitle" "--checklist" "Select the user accounts you wish to configure..." "${windowsize[@]}" "${userlist[@]}")
+	dialog_param=("--separate-output" "--backtitle" "$backtitle" "--checklist" "Select the user accounts you wish to configure RDP on" "${windowsize[@]}" "${userlist[@]}")
 	selectedusers=$(dialog "${dialog_param[@]}" 2>&1 >/dev/tty)
   echo allusers = $allusers
 
@@ -189,7 +119,6 @@ select_local_user_accounts_to_config()
   rm ./usernames.tmp
 }
 
-
 # creates a .xsession file for each selected local user account
 # based on the selected desktop environment
 create_xsession()
@@ -197,36 +126,13 @@ create_xsession()
 (		for username in $selectedusers
 		do
 			homedir=`grep "^$username:" /tmp/passwd | cut -d":" -f6`
-			echo "Creating .xsession file for $username in $homedir with entry \"$session\".." 2>&1
-			echo $session > $homedir/.xsession
+			echo "Creating .xsession file for $username in $homedir" 2>&1
+			echo "mate-session" > $homedir/.xsession
 			usergroup=`id -gn $username`
 			chown $username:$usergroup $homedir/.xsession
 			chmod u+x $homedir/.xsession
 		done) | dialog --backtitle "$backtitle" --title "creating .xsession files..." --progressbox "Processing..." 12 80
 		sleep 3
-}
-
-select_desktop()
-{
-	title="RDPsesconfig"
-	backtitle="Scarygliders RDPsesconfig"
-	windowsize=(0 0 0)
-	dialog_param=("--backtitle" "$backtitle" "--radiolist" "$title" "${windowsize[@]}" "${desktoplist[@]}")
-	desktop=$($DIALOG "${dialog_param[@]}" 2>&1 >/dev/tty)
-}
-
-
-
-# Configure a gnome environment
-
-
-# configure a unity environment (2d)
-config_for_mate_on_ubuntu()
-{
-    session="mate-session"
-    selecttext="Select which user(s) to configure a MATE session for..."
-    RequiredPackages=(mate-core mate-desktop-environment)
-    
 }
 
 
@@ -236,35 +142,10 @@ config_for_mate_on_ubuntu()
 
 ##############################################
 ######## Main routine starts here ############
-##############################################
-
-# Source the common functions...
-DIALOG="dialog"
-
-case "$supported" in
-	"1")
-		dialogtext="\nWelcome to the ScaryGliders RDPsesconfig script.\n\nThe detected distribution is : $Dist \non which this utility has been tested and supports.\n\nClick OK to continue...\n"
-		info_window
-		;;
-	"0")
-		dialogtext="\nWelcome to the ScaryGliders RDPsesconfig script.\n\nThe detected distribution is : $Dist .\n\nUnfortunately, no testing has been done for running this utility on this distribution.\n\nIf this is a Debian-based distro, you can try running it, but it might not work.\n\nIf the utility does work on this distribution, please let the author know!\n\nIf you wish to proceed, then click OK. Otherwise click Cancel to stop right here."
-		info_window
-		;;
-esac
-
-#create_desktop_dialog_list
-select_desktop
-
-case "$desktop" in
-	"MATE")
-        config_for_mate_on_ubuntu
-	    ;;
-esac
-
-#install_required_packages # Check if packages for selected desktop are installed and install if not.	
+##############################################	
 select_local_user_accounts_to_config
 create_xsession
 rm /tmp/passwd
 
-dialogtext="\nAll selected operations are complete!\n\nThe users you configured will be able to log in via RDP now and be presented with the desktop you configured for them.\n\nIf you wish for RDP users to be able to perform certain tasks like \"local\" users, please see the configuration files located at /usr/share/polkit-1/actions/ .\n\nSee http://scarygliders.net for details on PolicyKit.\n\nClick OK to exit the utility.\n\n\nThank you for using the Scarygliders RDPsesconfig!\n"
+dialogtext="\nAll selected operations are complete!\n\nThe users you configured will be able to log in via RDP now and be presented with the desktop you configured for them.\n\nIf you wish for RDP users to be able to perform certain tasks like \"local\" users, please see the configuration files located at /usr/share/polkit-1/actions/ .\n\nSee http://scarygliders.net for details on PolicyKit.\n\nClick OK to exit the utility.\n\n"
 info_window
